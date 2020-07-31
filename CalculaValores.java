3d915afd66be093790096d05f13190a67d80bc2b
Jessica Souza <kikasouza@gmail.com>
2020-07-30 22:01:41 -0300

    enviando Readme e alteracoes em CalculaValores.java

--- CalculaValores.java
+++ CalculaValores.java
@@ -1,6 +1,6 @@
 public class CalculaValores {
 
-	public static void main (final String[] args) {
+	public static void main (String[] args) {
 
 		int numeroUm;
 		int numeroDois;
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Projetos de lógica de programação em Java
