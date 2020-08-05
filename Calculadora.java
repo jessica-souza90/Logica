7e29aa220d1bc9b1f55f90def3890e56029ef81a
Jessica Souza <kikasouza@gmail.com>
2020-08-04 21:28:04 -0300

    alteração de calculadora.java e inclusão mediaaritmetica.java

--- Calculadora.java
+++ Calculadora.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class Calculadora {
   
  	public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in); 
+        
+        double n1;
+        double n2;
+
+        double soma;
+        double subtracao;
+        double multiplicacao;
+        double divisao;
+
+        Scanner sc = new Scanner(System.in);
                 
         System.out.print("Digite o numero: ");
-        double numero1 = sc.nextDouble();
+        n1 = sc.nextDouble();
 
         System.out.print("Digite o numero: ");
-        double numero2 = sc.nextDouble();
+        n2 = sc.nextDouble();
 
-        double soma = numero1 + numero2;
-        double subtracao = numero1 - numero2;
-        double multiplicacao = numero1 * numero2;
-        double divisao = numero1 / numero2;
+        soma = n1 + n2;
+        subtracao = n1 - n2;
+        multiplicacao = n1 * n2;
+        divisao = n1 / n2;
 
         System.out.println("Resultado da soma: " + soma);
-		System.out.println("Resultado da subtracao " + subtracao);
+        System.out.println("Resultado da subtracao " + subtracao);
         System.out.println("Resultado da multiplicacao " + multiplicacao);
         System.out.println("Resultado da divisao " + divisao);
 
