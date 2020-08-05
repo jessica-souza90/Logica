b8ebced7efef6a1a6e76fdb5f4f6712c56d3a85c
Jessica Souza <kikasouza@gmail.com>
2020-08-04 22:44:40 -0300

    alterações em MediaAritmetica

--- MediaAritmetica.java
+++ MediaAritmetica.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 
 public class MediaAritmetica {
   
- 	public static void main (String[] args) {
+    public static void main (String[] args) {
     
-    double num1;
-    double num2;
-    double num3;
-    double num4;
-    double soma;
-    double media;
+        double num1;
+        double num2;
+        double num3;
+        double num4;
+        double soma;
+        double media;
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
     
-    System.out.println("Escreva primeiro numero:");
-    num1 = sc.nextDouble(); 
+        System.out.println("Escreva primeiro numero:");
+        num1 = sc.nextDouble(); 
 
-    System.out.println("Escreva segundo numero:");
-    num2 = sc.nextDouble(); 
+        System.out.println("Escreva segundo numero:");
+        num2 = sc.nextDouble(); 
 
-    System.out.println("Escreva terceiro numero:");
-    num3 = sc.nextDouble(); 
+        System.out.println("Escreva terceiro numero:");
+        num3 = sc.nextDouble(); 
 
-    System.out.println("Escreva quarto numero:");
-    num4 = sc.nextDouble(); 
+        System.out.println("Escreva quarto numero:");
+        num4 = sc.nextDouble(); 
 
-    soma = num1 + num2 + num3 + num4;
-    media = soma / 4;
+        soma = num1 + num2 + num3 + num4;
+        media = soma / 4;
 
-    System.out.println("A media e: " + media);
+        System.out.println("A media e: " + media);
 
         sc.close();
         
